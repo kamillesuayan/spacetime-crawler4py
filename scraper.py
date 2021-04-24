8b7ae1ad9eabcba693dabf912ec71e6cecefd2dd
kamillesuayan <kamille.suayan@gmail.com>
2021-04-24 12:31:54 -0700

    experimenting with simhash

--- scraper.py
+++ scraper.py
@@ -27,36 +27,34 @@ def get_features(s):
     return [s[i:i + width] for i in range(max(len(s) - width + 1, 1))]
 
 objs = dict()
-index = SimhashIndex(objs, k=10) # create index
+index = SimhashIndex(objs, k=6) # create index
 
 def scraper(url, resp):
     # do something with resp
     if resp.status >= 200 and resp.status <= 202 and (url not in links_visited):
-        reqs = requests.get(url,timeout=15)
+        reqs = requests.get(url,timeout=60)
         parsed = urlparse(url)
         if parsed.fragment != None and parsed.fragment != "":
             url = urldefrag(url)[0] # defragments the URL that is the parameter
             # print("AHAHA: ", url)
         soup = BeautifulSoup(reqs.text, 'html.parser')
         wrds = soup.get_text()
-        
-        if url in crawler.unique_URLs:
-            return
 
         # 1. for unique URLs
-        crawler.unique_URLs.add(url)
-        if len(wrds.split()) < 200 or len(wrds.split()) > 50000:
-            return
         
+        
+        crawler.len_info.write(f"{url}\n")
+        
+        crawler.unique_URLs.add(url)
         tkns = tkn.tokenize(wrds, stp_wrds)
 
         if len(tkns) >= 200 and len(tkns) <= 50000:
             s1 = Simhash(get_features(wrds))
-            if len(index.get_near_dups(s1)) > 1:
-                print(url,len(index.get_near_dups(s1)))
+            if len(index.get_near_dups(s1)) > 2:
+                links_visited.add(url)
                 return
-            index.add(len(crawler.unique_URLs), s1)
-            crawler.len_info.write(f"{len(tkns)}\n")
+            index.add(len(links_visited), s1)
+            
             # 2. keep track of longest page in terms of words
             if crawler.longest[0] < len(tkns):
                 crawler.longest[0] = len(tkns)
@@ -77,14 +75,18 @@ def scraper(url, resp):
                 else:
                     crawler.subdomains[new_url] += 1
 
+            links_visited.add(url)
             links = extract_next_links(url, resp)
+            print("     Links Visited Length:", len(links_visited))
+            print("     Unique Links:", len(crawler.unique_URLs))
             return [link for link in links if is_valid(link)]
+    links_visited.add(url)
     return
 
 def extract_next_links(url, resp):
     # parsed = urlparse(url)
     urls = []
-    reqs = requests.get(url,timeout=15)
+    reqs = requests.get(url,timeout=60)
     soup = BeautifulSoup(reqs.text, 'html.parser')
     # base = "https://" + parsed.netloc
     for link in soup.find_all('a'): # gets all the links that are on the webpage
